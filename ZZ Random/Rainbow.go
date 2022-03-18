57069cbe556cfa6aff776dcef89b2ed9e42b6859
Rodric <72948934+RodricBr@users.noreply.github.com>
2022-03-18 20:52:54 -0300

    Create Rainbow.go

---
+++ ZZ Random/Rainbow.go
@@ -0,0 +1,25 @@
+package main
+
+// Created by a random guy I saw: github.com/Yariya
+
+import ("fmt"; "math/rand"; "os"; "time")
+
+func rainbow(i string, timeout int) {
+	colors := []string{
+		"\033[;31m",
+		"\033[;34m",
+		"\033[;32m",
+		"\033[;35m",
+    // Adicionar mais cores na sequencia...
+	}
+	for {
+		n := rand.Int() % len(colors)
+		fmt.Printf("\r%s%s", colors[n], i)
+		os.Stdout.Sync()
+		time.Sleep(time.Millisecond * time.Duration(timeout))
+	}
+
+}
+func main() {
+	rainbow("Olá, mundo", 300) // Texto & Timeout
+}
